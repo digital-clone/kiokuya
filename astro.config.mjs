a69239264d630d1e95bd1cd6f61cbb17610538ac
kuniyan <57828407+kuni-0930@users.noreply.github.com>
2026-03-24 16:56:19 +0900

    add starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,20 @@
 import { defineConfig } from 'astro/config';
 
+import starlight from '@astrojs/starlight';
+
 export default defineConfig({
   site: 'https://digital-clone.github.io',
   base: '/kiokuya',
+  outDir: ".docs",
+  integrations: [
+    starlight({
+        title: "DigitalCloneV",
+        sidebar: [
+            {
+                label: "導入方法",
+                autogenerate: {directory: "install"},
+            },
+        ],
+    }
+    )],
 });
---
+++ src/content/docs/install/install.md
@@ -0,0 +1,8 @@
+---
+title: DigitalCloneV
+description: DigitalCloneVの導入
+---
+
+## ツールの導入
+
+- unitypackage を Unity にドラッグ＆ドロップしてください。<br>
